d31aef028d53c377141205147f0ca16f4f1e8104
Mark <markandrewhamm@gmail.com>
2020-07-30 20:52:54 -0400

    loaded moment library

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello Internet");
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+console.log(moment().format('LLL'));
+
+console.log(moment().subtract(6, 'days').calendar());
+
+// console.log(Date.parse("t"));
